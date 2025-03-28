8a7c8d6b3e8320fcc83e6a4c7a7197e5cad925be
farhan <farhanalirazaazeemi@gmail.com>
2025-03-28 22:27:35 +0500

    removed unused code

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,6 @@ struct Args {
     extensions: Option<String>,
 }
 
-fn is_hidden(entry: &DirEntry) -> bool {
-    entry
-        .file_name()
-        .to_str()
-        .map(|s| s.starts_with("."))
-        .unwrap_or(false)
-}
-
 fn should_skip_path(path: &str) -> bool {
     let components: Vec<&str> = path.split('/').collect();
     println!("Checking path: {}", path);
